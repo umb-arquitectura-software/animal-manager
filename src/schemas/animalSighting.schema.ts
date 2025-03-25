64a092311f4549ade940f0764808d9b6ad96572a
Harold Camargo <hscamargo2002@gmail.com>
2025-03-25 08:42:35 -0500

    ⚡️docker configuration

--- src/schemas/animalSighting.schema.ts
+++ src/schemas/animalSighting.schema.ts
@@ -1,30 +1,28 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import mongoose, { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from "mongoose";
 
 export type AnimalSightingDocument = HydratedDocument<AnimalSighting>;
 
-
 @Schema({ timestamps: true })
 export class AnimalSighting {
+  
+  @Prop({ 
+    type: { userId: String, username: String }, 
+    required: true 
+  })
+  user: { userId: string; username: string };
 
-  @Prop({ required: true })
-  user: {
-    userId: string;
-    username: string;
-  }
+  @Prop({ 
+    type: { animalId: String, name: String, species: String }, 
+    required: true 
+  })
+  animal: { animalId: string; name: string; species: string };
 
-  @Prop({ required: true })
-  animal: {
-    animalId: string;
-    name: string;
-    species: string;
-  }
-
-  @Prop({ required: true })
+  @Prop({ type: [Number], required: true })
   coordinates: number[];
 
-  @Prop({ required: false })
-  img: string;
+  @Prop({ type: String, required: false })
+  img?: string;
 }
 
 export const AnimalSightingSchema = SchemaFactory.createForClass(AnimalSighting);
